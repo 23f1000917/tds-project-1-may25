bda3becae70053bd18395f3b8781997c1f240bf3
23f1000917 <23f1000917@ds.study.iitm.ac.in>
2025-06-18 21:56:15 +0530

    Update app.py
    add cors

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ from solution_creator import SolutionCreator
 
 app = Flask(__name__)
 
-
+@app.after_request
+def add_cors(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"  
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"  
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"  
+    return response
 
 @app.get('/')
 def index():
