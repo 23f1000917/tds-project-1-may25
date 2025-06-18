0ae3f07ed7f5a8ff8b941de03cb98c6d71effcf5
23f1000917 <23f1000917@ds.study.iitm.ac.in>
2025-06-18 22:41:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, request, jsonify
 from solution_creator import SolutionCreator
+from flask_cors import CORS  # Import CORS
+
 
 
 app = Flask(__name__)
-
-@app.after_request
-def add_cors(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"  
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"  
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"  
-    return response
+CORS(app)
 
 @app.post('/api') 
 def api():
